01e77f6b77ec9a61e0922fb3a6c8a7ba92df1d15
Asher Alley <asher.asheralley@gmail.com>
2023-01-26 21:21:23 +1300

    updated character model

--- backend/models/characterModel.js
+++ backend/models/characterModel.js
@@ -5,32 +5,58 @@ const characterSchema = mongoose.Schema(
     user: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
-      ref: 'User',
+      ref: 'User'
     },
-    stats: {
-      type: Array,
-      "default": [],
-      required: [true, 'Please add a text value'],
+    characterName: {
+      type: String
     },
-    equipement: [
-      {
-        description: String,
-        resistance: String,
-      }
-    ],
-    leftHand: {
-      type: String,
-      baseDamage: String,
-      bonus: String,
+    race: {
+      type: String
+    },
+    class: {
+      type: String
+    },
+    subClass: {
+      type: String
+    },
+    mainStats: {
+      strength: {type: Number},
+      dexterity: {type: Number},
+      constitution: {type: Number},
+      intelligence: {type: Number},
+      wisdom: {type: Number},
+      charisma: {type: Number}
+    },
+    savingThrows: {
+      strength: {type: Number},
+      dexterity: {type: Number},
+      constitution: {type: Number},
+      intelligence: {type: Number},
+      wisdom: {type: Number},
+      charisma: {type: Number}
+    },
+    hp: {type: Number},
+    tempHp: {type: Number},
+    hitDice: {type: String},
+    armourClass: {type: Number},
+    deathSaves: {
+      successes: {type: Number},
+      fails: {type: Number}
+    },
+    abilitySlots: {
+      level: {type: String},
+      totalSlotNumber: {type: Number},
+      used: {type: Number},
     },
     money: {
-      type: Array,
-      "default": [],
+      GP: {type: Number},
+      SP: {type: Number},
+      Plt: {type: Number}
     }
   },
   {
     timestamps: true,
   }
-)
+);
 
 module.exports = mongoose.model('Character', characterSchema)
